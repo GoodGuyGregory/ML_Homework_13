a8c37a9b967c190a12ec2735801924f730c35f2e
GoodGuyGregory <greg.witt625@gmail.com>
2024-02-13 01:12:39 -0500

    adds logic to determine the best trail by eyeballing the resulting dictionary from a GD algorithmn implmentation

--- main.py
+++ main.py
@@ -1,40 +1,72 @@
+import copy
+import math
 import random
-import np
+
 
 def gradientDescent(eta, xVal, yVal):
     calculatedValues = []
-    xSubT = xVal - eta * (-4 + 4(xVal))
-    ySubT = yVal - eta * (24 + 6(yVal))
+    xSubT = xVal - eta * (-4 + (4 * (xVal)))
+    ySubT = yVal - eta * (24 + (6 * (yVal)))
     calculatedValues.append(xSubT)
     calculatedValues.append(ySubT)
     return calculatedValues
 
-    def main():
-        # rates for each trial
-        eta = [.1, .01, .001]
+def determinePerformance(xVal, yVal):
+    performanceMetric = {}
+    xValBest = (-4 + (4 * (xVal)))
+    yValBest = 24 + (6 * (yVal))
+    performanceMetric[xVal] = round(xValBest,4)
+    performanceMetric[yVal] = round(yValBest,4)
+#   returns trial values for xVal, yVal
+    return performanceMetric
 
 
 
-        # initialize random inputs.
-        #  iterate through each trial.
-        for rate in eta:
-            # calculated based on partial derivates
-            bestX = 1
-            bestY = -4
-            for i in range(10):
-                # generate a random starting point
-                xValue = random.randint(-10, 10)
-                yValue = random.randint(-10, 10)
+def main():
+    # rates for each trial
+    eta = [.1, .01, .001]
 
-                for i in range(500):
-                    xYDeltas = gradientDescent(rate, xValue, yValue)
-                    xValue = xYDeltas[0]
-                    yValue = xYDeltas[1]
+    # create dictionary for corresponding trails and values of x and y for performance metrics
+    etaTrials = {}
 
-    #              plug them back in and determine best performance.
+    # build the initial
+    for etaRates in eta:
+        # initialize an empty list for performance metrics per the 10 trails
+        etaTrials[etaRates] = []
 
+    # initialize random inputs.
+    #  iterate through each trial.
+    for rate in eta:
+        # calculated based on partial derivates
+        # trials list
+        trialList = []
+        for i in range(10):
+            # generate a random starting point
+            xValue = random.randint(-10, 10)
+            yValue = random.randint(-10, 10)
 
+            originalxValue = copy.deepcopy(xValue)
+            originalyValue = copy.deepcopy(yValue)
 
+            for i in range(500):
+                xYDeltas = gradientDescent(rate, xValue, yValue)
+                xValue = xYDeltas[0]
+                yValue = xYDeltas[1]
+
+        # plug them back in and determine the best performance.
+            trialList.append(determinePerformance(xValue, yValue))
+        # determine the best performaning trial and record the results in the dictionary.
+        etaTrials[rate].append(trialList)
+
+    #  print each trial for analysis:
+    for k,v in etaTrials.items():
+        print('Eta:' + str(k))
+        print("======================================")
+        print("Trial Results:")
+        for trial in range(len(v)):
+            print(v)
+
+    # append the best one to the list of etaTrails map.
 
     # perform GD 500 times
 
