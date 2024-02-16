05ac9107edbe96997398a6a316c0bf9e4a18e398
GoodGuyGregory <greg.witt625@gmail.com>
2024-02-16 09:20:32 -0500

    adds evaluation metric function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def gradientDescent(eta, xVal, yVal):
     calculatedValues.append(ySubT)
     return calculatedValues
 
-def determinePerformance(xVal, yVal):
+def determinePerformanceForMinimum(xVal, yVal):
     performanceMetric = {}
     xValBest = (-4 + (4 * (xVal)))
     yValBest = 24 + (6 * (yVal))
@@ -17,6 +17,9 @@ def determinePerformance(xVal, yVal):
     performanceMetric[yVal] = round(yValBest,4)
 #   returns trial values for xVal, yVal
     return performanceMetric
+
+def determineGlobalPerformance(xVal, yVal):
+    return 52 - (4 * xVal) + (2 * (xVal ** 2)) + (24 * yVal) + (3 * (yVal**2))
 
 
 
@@ -49,7 +52,7 @@ def main():
                 yValue = xYDeltas[1]
 
         # plug them back in and determine the best performance.
-            trialList.append(determinePerformance(xValue, yValue))
+            trialList.append(determinePerformanceForMinimum(xValue, yValue))
         # determine the best performaning trial and record the results in the dictionary.
         etaTrials[rate].append(trialList)
 
@@ -66,4 +69,28 @@ def main():
     # perform GD 500 times
 
 
+
 main()
+
+def evaluate():
+    eta = [.1, .01, .001]
+    for i in range(len(eta)):
+        # find and supply the best value:
+        print("supply the best trial results for " + str(eta[i]))
+        print("==============================================")
+        xVal = input("provide best X: ")
+        yVal = input("provide best Y: ")
+        print("Performance to Global min of 2")
+        print(determineGlobalPerformance(float(xVal), float(yVal)))
+
+
+evaluate()
+
+
+# Comments the higher the eta value the results were able to be closer and more refined to an approximate minimum as each trial
+# was executed. first time time around each of the trials ranged the smaller the eta the more unpredictable the resulting minumim value.
+# the program runs and produces key value pairs for x and y <x: <minum with respect to its derivative> and <y: <with respect to it's derivative>
+# to see the results of the best performance execute the code and supply the best x and y keys based on their values with respect their global mins
+
+
+
